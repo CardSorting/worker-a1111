6db5f61ed57c02f99c6b3a3306501d03480bb20c
Card Sorting <115497755+CardSorting@users.noreply.github.com>
2023-07-24 20:32:48 -0600

    Update rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,112 +1,124 @@
 import os
-import logging
 import time
 import requests
-from b2sdk.v1 import InMemoryAccountInfo, B2Api, FileVersionInfo
+from b2sdk.v1 import InMemoryAccountInfo, B2Api
 from requests.adapters import HTTPAdapter, Retry
 import runpod
 
-# Setup logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Setup automatic session
+automatic_session = requests.Session()
+retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
+automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
 
 
-class HTTPRequestHandler:
-    """
-    Handles HTTP requests with automatic retries for intermittent network failures.
-    """
-    def __init__(self):
-        self.session = requests.Session()
-        retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
-        self.session.mount('http://', HTTPAdapter(max_retries=retries))
+# ---------------------------------------------------------------------------- #
+#                              Automatic Functions                             #
+# ---------------------------------------------------------------------------- #
 
-    def request(self, method, url, timeout=600, json=None):
-        """
-        Performs the HTTP request and returns the response as JSON.
-        """
-        response = self.session.request(method, url, timeout=timeout, json=json)
-        return response.json()
+def wait_for_service(url):
+    '''
+    Check if the service is ready to receive requests.
+    '''
+    while True:
+        try:
+            requests.get(url)
+            return
+        except requests.exceptions.RequestException:
+            print("Service not ready yet. Retrying...")
+        except Exception as err:
+            print("Error: ", err)
 
-    def wait_for_service(self, url):
-        """
-        Polls the service at the given URL until it is ready to receive requests.
-        """
-        while True:
-            try:
-                self.session.get(url)
-                return
-            except requests.exceptions.RequestException:
-                logger.info("Service not ready yet. Retrying...")
-                time.sleep(0.2)
+        time.sleep(0.2)
 
 
-class B2Bucket:
-    """
-    Provides a wrapper for interacting with a Backblaze B2 bucket.
-    """
-    def __init__(self, account_id, application_key, bucket_name):
-        info = InMemoryAccountInfo()
-        self.b2_api = B2Api(info)
-        self.b2_api.authorize_account('production', account_id, application_key)
-        self.bucket = self.b2_api.get_bucket_by_name(bucket_name)
+def run_inference(params, api_config):
+    api_name = params["api_name"]
+    path = None
 
-    def upload_file(self, file_path, file_name=None):
-        """
-        Uploads a file to the B2 bucket.
-        """
-        file_name = file_name or os.path.basename(file_path)
-        with open(file_path, 'rb') as file:
-            try:
-                logger.info(f"Uploading {file_name} to Backblaze B2...")
-                file_info = self.bucket.upload_bytes(file.read(), file_name)
-                logger.info(f"Upload successful: {file_info.file_name}")
-                return file_info
-            except Exception as e:
-                logger.error(f"Failed to upload {file_name} to Backblaze B2: {e}")
-                return None
+    if api_name in api_config["api"]:
+        api_config = api_config["api"][api_name]
+    else:
+        raise Exception("Method '%s' not yet implemented")
+
+    api_verb = api_config[0]
+    api_path = api_config[1]
+
+    response = {}
+
+    if api_verb == "GET":
+        response = automatic_session.get(
+                url='%s%s' % (api_config["baseurl"], api_path),
+                timeout=api_config["timeout"])
+
+    if api_verb == "POST":
+        response = automatic_session.post(
+                url='%s%s' % (api_config["baseurl"], api_path),
+                json=params, 
+                timeout=api_config["timeout"])
+
+    return response.json()
 
 
-def handler(event, api_config, request_handler, b2_bucket):
-    """
-    Performs API inference and uploads the result to B2 bucket.
-    """
-    api_name = event["input"]["api_name"]
-    api_verb, api_path = api_config["api"][api_name]
-    url = f"{api_config['baseurl']}{api_path}"
-    response = request_handler.request(api_verb, url, json=event["input"])
+def initialize_b2(account_id, application_key):
+    info = InMemoryAccountInfo()
+    b2_api = B2Api(info)
+    b2_api.authorize_account('production', account_id, application_key)
+    return b2_api
 
-    # Save the response to a file (assuming it's text data)
+
+def upload_to_b2(b2_api, bucket_name, file_path, file_name=None):
+    file_name = file_name or os.path.basename(file_path)
+    bucket = b2_api.get_bucket_by_name(bucket_name)
+    with open(file_path, 'rb') as file:
+        file_info = bucket.upload_bytes(file.read(), file_name)
+        return file_info
+
+
+# ---------------------------------------------------------------------------- #
+#                                RunPod Handler                                #
+# ---------------------------------------------------------------------------- #
+
+def handler(event, api_config, b2_api, bucket_name):
+    '''
+    This is the handler function that will be called by the serverless.
+    '''
+
+    json = run_inference(event["input"], api_config)
+
+    # Save the response to a file
     with open('response.txt', 'w') as file:
-        file.write(str(response))
+        file.write(str(json))
 
     # Upload the file to Backblaze B2
-    file_info = b2_bucket.upload_file('response.txt')
+    file_info = upload_to_b2(b2_api, bucket_name, 'response.txt')
 
     # Return the URL of the uploaded file
     if file_info is not None:
-        return [f"https://s3.us-east-005.backblazeb2.com/file/{b2_bucket.bucket.name}/{file_info.file_name}"]
+        return [f"https://s3.us-east-005.backblazeb2.com/file/{bucket_name}/{file_info.file_name}"]
 
 
-# Define API configuration
-api_config = {
-    "baseurl": "http://127.0.0.1:3000",
-    "api": {
-        "txt2img":  ("POST", "/sdapi/v1/txt2img"),
-        "img2img":  ("POST", "/sdapi/v1/img2img"),
-        "getModels": ("GET", "/sdapi/v1/sd-models"),
-        "getOptions": ("GET", "/sdapi/v1/options"),
-        "setOptions": ("POST", "/sdapi/v1/options"),
-    },
-    "timeout": 600
-}
-
-# Initialize request handler and B2 bucket
-request_handler = HTTPRequestHandler()
-b2_bucket = B2Bucket(os.getenv('B2_ACCOUNT_ID'), os.getenv('B2_APP_KEY'), os.getenv('B2_BUCKET_NAME'))
-
-# Start the serverless service
-runpod.serverless.start({"handler": lambda event: handler(event, api_config, request_handler, b2_bucket)})
+# ---------------------------------------------------------------------------- #
+#                                    Main                                      #
+# ---------------------------------------------------------------------------- #
 
 if __name__ == "__main__":
-    request_handler.wait_for_service(url='http://127.0.0.1:3000/sdapi/v1/txt2img')
-    logger.info("WebUI API Service is ready. Starting RunPod...")
+    api_config = {
+        "baseurl": "http://127.0.0.1:3000",
+        "api": {
+            "txt2img":  ("POST", "/sdapi/v1/txt2img"),
+            "img2img":  ("POST", "/sdapi/v1/img2img"),
+            "getModels": ("GET", "/sdapi/v1/sd-models"),
+            "getOptions": ("GET", "/sdapi/v1/options"),
+            "setOptions": ("POST", "/sdapi/v1/options"),
+        },
+        "timeout": 600
+    }
+
+    wait_for_service(url='http://127.0.0.1:3000/sdapi/v1/txt2img')
+
+    print("WebUI API Service is ready. Starting RunPod...")
+
+    b2_api = initialize_b2(os.getenv('B2_ACCOUNT_ID'), os.getenv('B2_APP_KEY'))
+    bucket_name = os.getenv('B2_BUCKET_NAME')
+
+    runpod.serverless.start({"handler": lambda event: handler(event, api_config, b2_api, bucket_name)})
