7eb240cddd57c4807a58567424b7e5bedf041f6d
Card Sorting <115497755+CardSorting@users.noreply.github.com>
2023-07-24 23:49:49 -0600

    Update rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,8 +1,10 @@
+import os
 import time
-
-import runpod
+import base64
 import requests
+from b2sdk.v1 import InMemoryAccountInfo, B2Api
 from requests.adapters import HTTPAdapter, Retry
+import runpod
 
 automatic_session = requests.Session()
 retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
@@ -68,6 +70,24 @@ def run_inference(params):
 
     return response.json()
 
+def base64_to_image(base64_string, output_path):
+    image_data = base64.b64decode(base64_string)
+    with open(output_path, 'wb') as file:
+        file.write(image_data)
+
+def initialize_b2(account_id, application_key):
+    info = InMemoryAccountInfo()
+    b2_api = B2Api(info)
+    b2_api.authorize_account('production', account_id, application_key)
+    return b2_api
+
+def upload_to_b2(b2_api, bucket_name, file_path, file_name=None):
+    file_name = file_name or os.path.basename(file_path)
+    bucket = b2_api.get_bucket_by_name(bucket_name)
+    with open(file_path, 'rb') as file:
+        file_info = bucket.upload_bytes(file.read(), file_name)
+        return file_info
+
 
 # ---------------------------------------------------------------------------- #
 #                                RunPod Handler                                #
@@ -76,12 +96,27 @@ def handler(event):
     '''
     This is the handler function that will be called by the serverless.
     '''
+    response = run_inference(event["input"])
+    
+    # Check if 'images' is in the response and throw a meaningful error if not
+    if "images" not in response or not response["images"]:
+        raise Exception("The response from run_inference does not contain 'images' key or the 'images' list is empty.")
 
-    json = run_inference(event["input"])
+    # The base64 string is the first item in the 'images' list
+    base64_string = response["images"][0]
 
-    # return the output that you want to be returned like pre-signed URLs to output artifacts
-    return json
+    # Decode base64 to image
+    base64_to_image(base64_string, 'output_image.png')
 
+    # Initialize B2 API
+    b2_api = initialize_b2(os.getenv('B2_ACCOUNT_ID'), os.getenv('B2_APP_KEY'))
+
+    # Upload the image to Backblaze B2
+    file_info = upload_to_b2(b2_api, os.getenv('B2_BUCKET_NAME'), 'output_image.png')
+
+    # Return the URL of the uploaded file
+    if file_info is not None:
+        return [f"https://s3.us-east-005.backblazeb2.com/file/{os.getenv('B2_BUCKET_NAME')}/{file_info.file_name}"]
 
 if __name__ == "__main__":
     wait_for_service(url='http://127.0.0.1:3000/sdapi/v1/txt2img')
